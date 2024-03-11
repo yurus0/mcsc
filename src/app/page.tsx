a1a07e4bd69211843b1b2cbb2deb3bd92a519076
yurus0 <imanebenrazzouk0302@gmail.com>
2024-03-11 18:28:50 +0000

    feat: adding faq; adding partners;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import FAQ from '@/components/Faq';
 import SpaceParticles from '@/components/SpaceParticles';
-import TypingEffect from '@/components/TypingEffect';
 import Image from 'next/image';
 import { Canvas } from 'react-three-fiber';
 import Navbar from '../components/Navbar';
@@ -19,22 +18,102 @@ export default function Home() {
       <div className='sticky'>
       <Navbar/>
       </div>
-      <div className="flex min-h-screen flex-col items-center justify-between p-20">
-        <div className="flex pt-4">
+      <div className="flex min-h-80 flex-col items-center justify-between">
+        <div className="flex pt-40">
           <div className="flex justify-center">
             <Image
               src="/title.svg"
               alt="Title"
-              width={800}
-              height={500}
-              priority
+              width={1000}
+              height={100}
             />
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center pt-10">
-          {/*typewriter effect*/}
-          <TypingEffect text="Welcome to MCSC 2022" />
+        <div className='flex flex-row justify-between'>
+        <div className=''>
+          <h1 className="text-2xl font-semibold text-right">
+            <code>23, 24, 25 april 2024, Rabat - MA</code>
+            </h1>
+        </div>
+        </div>
+    </div>
+    <div className='py-10'></div>
+    <div className="flex min-h-screen flex-row items-center justify-between p-10">
+      {/*about*/}
+      <div className="flex flex-col">
+        <h1 className="text-4xl font-semibold text-left mb-8 pt-6">
+          <code>Gathering Enthusiasts !</code>
+        </h1>
+        <div className="items-start mx-auto pl">
+          <p className="text-sm font-light">
+            <code>
+            is an annual gathering that unites cybersecurity enthusiasts, industry experts, researchers, and students from Morocco and beyond. This event is designed to foster cybersecurity awareness, facilitate knowledge exchange, and serve as a unique platform for skill development.
+            </code>
+          </p>
+        </div>
+    </div>
+    <Image
+            src="/mcsc.png"
+            alt="About"
+            width={500}
+            height={500}
+            priority
+          />
+    <Image
+      src="/mcsc.png"
+      alt="About"
+      width={100}
+      height={500}
+      priority/>
+    </div>
+    <div className='relative flex min-h-screen flex-col items-center justify-center'>
+    <div style={{ height: '80vh', width: '100vw' }}>
+          <Canvas>
+            <SpaceParticles />
+          </Canvas>
+    </div>
+    <div className='absolute top-40 py-16'>
+        <h1 className="text-4xl font-semibold text-center">
+          <code>Partners</code>
+          </h1>
+    </div>
+    <div className='absolute flex bg-gradient-to-t from-transparent from-10% via-[#00ff41]/[.4] to-90% opacity-15 h-28 w-full'>
+    </div>
+    <div className='absolute flex flex-col bg-gradient-to-t from-transparent via-[#00ff41]/[.35] min-h-72 w-full justify-center items-center'>
+    
+      <div className='flex flex-row justify-center items-center'>
+        <div className='px-10'>
+        <Image
+          src="/CC.svg"
+          alt="cybercohesion"
+          width={200}
+          height={150}
+          priority
+        /></div>
+      <div className='px-10'>
+        <Image
+          src="/offsec.png"
+          alt="offsec"
+          width={400}
+          height={250}
+          priority
+        />
+        </div>
+      <div className='px-10'>
+        <Image
+          src="/aws.png"
+          alt="aws"
+          width={300}
+          height={200}
+          priority
+        /></div>
       </div>
+    </div>
+    <div className='absolute flex min-h-60 w-full'>
+      
+    </div>
+    </div>
+    <div className="flex min-h-screen flex-col items-center justify-between p-20">
       <FAQ />
     </div>
     </div>
--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -6,9 +6,13 @@ import { useState } from 'react';
 const FAQ = () => {
     const [faqData, setFaqData] = useState([
         { question: 'What is the Cyber Security Camp Morocco?', answer: 'It is the oldest largest open cyber security event in Morocco. This year we are celebrating the beginning of a new era with the 11th edition.', isOpen: false },
-        { question: 'ho can participate in the event?', answer: 'Everyone is welcome to attend. However, as the Cyber Security Camp hosts a CTF competition, your team and you can participate only if you successfully passed the qualification phase before.', isOpen: false },
-        { question: 'Question?', answer: 'Answer', isOpen: false },
-        { question: 'Is there any entrance fee?', answer: 'Answer', isOpen: false },
+        { question: 'Who can participate in the event?', answer: 'We welcome attendees from diverse backgrounds, including students, professionals, educators, and anyone interested in cybersecurity. However, as the Cyber Security Camp hosts a CTF competition, your team and you can participate only if you successfully passed the qualification phase before.', isOpen: false },
+        { question: 'How can I register for the Cybersecurity Camp?', answer: 'Get you ticket now by completing the registration above.', isOpen: false },
+        { question: 'Do I need prior cybersecurity knowledge to attend?', answer: 'Answer', isOpen: false },
+        { question: 'Are there any registration fees for the event?', answer: 'For a standard visitor you will not be asked to pay any fee. But if you want to receive any of the goodies we are offering exclusively to our competition participants, our speakers and our guests of honour, make sure you reach out to us by messaging us directly via our instagram account or our email.', isOpen: false },
+        { question: 'Will there be hands-on activities or workshops?', answer: 'Yes, there will be numerous workshops covering a wide range of topics within the cybersecurity field.', isOpen: false },
+        { question: 'Who are the instructors or speakers at the Cybersecurity Camp?', answer: 'Answer', isOpen: false },
+        { question: 'How can I get involved as a sponsor or partner for the event?', answer: 'Answer', isOpen: false },
     ]);
     
     const toggleAnswer = (index:any) => {
@@ -18,18 +22,19 @@ const FAQ = () => {
         setFaqData([...faqData]);
     };
     return (
-        <div className='flex min-h-min flex-col w-full justify-center px-8 py-20 bg-transparent'>
+        <div className='w-full justify-center px-8 py-20'>
         {/* Frequently Asked questions */}
-        <div className='flex flex-col items-center'>
+        <div className='flex flex-col items-center pb-20'>
             <h1 className='text-4xl font-semibold text-center mb-8'>
             <code>Frequently Asked Questions</code>
             </h1>
-            <div className='flex flex-col items-start max-w-screen-md mx-auto'>
+        </div>
+        <div className='flex flex-col items-start px-20 mx-auto'>
             {faqData.map((faq, index) => (
                 <motion.div
                 key={index}
                 className="card-container"
-                initial={{ y: 200, opacity: 0 }}
+                initial={{ y: 100, opacity: 0 }}
                 whileInView={{
                     y: 0,
                     opacity: 100,
@@ -42,33 +47,30 @@ const FAQ = () => {
                 viewport={{ once: true, amount: 1 }}
             >
                 <div>
-                <div className='mb-4'>
-                    <div className='flex flex-row cursor-pointer py-4' >
-                        <div onClick={() => toggleAnswer(index)}>
+                    <div className='flex flex-row justify-between py-4' >
+                    <div onClick={() => toggleAnswer(index)}>
                     {faq.isOpen ? (
-                        <ChevronDownIcon className='w-6 h-6' />
+                        <ChevronDownIcon className='w-6 h-6 hover:cursor-pointer' />
                     ) : (
-                        <ChevronRightIcon className='w-6 h-6 transform transition-transform duration-300 ease-in-out' style={{ transform: faq.isOpen ? 'rotate(90deg)' : 'rotate(0)' }} />
+                        <ChevronRightIcon className='w-6 h-6 transform transition-transform duration-300 ease-in-out hover:cursor-pointer' style={{ transform: faq.isOpen ? 'rotate(90deg)' : 'rotate(0)' }} />
                     )}
                     </div>
-                    <div className='justify-between px-8  max-w-screen-sm'>
+                    <div className='justify-between px-8 min-w-2/3'>
                         <p className='text-xl font-regular'>
                         <code>{faq.question}</code>
                         </p>
+                        <p className='text-sm break-normal pt-6 pr-2'>
                         {faq.isOpen && (
-                        <p className='text-sm break-normal pt-3 pr-2'>
                             <code>{faq.answer}</code>
-                        </p>
                         )}
+                        </p>
                     </div>
                     </div>
-                </div>
                 </div>
             </motion.div>
             ))}
         </div>
         </div>
-    </div>
     );
 }
 export default FAQ;
