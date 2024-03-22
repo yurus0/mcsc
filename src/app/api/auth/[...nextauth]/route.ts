12a1c45cc0ad3e530926aa67b3f78d86ef54fea3
blacktache <adnanemandili@gmail.com>
2024-03-22 01:43:20 +0000

    configurer le system des tickets

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,11 +4,16 @@ import GithubProvider from "next-auth/providers/github";
 
 declare module 'next-auth' {
     interface Session {
+<<<<<<< HEAD
     user: {
+=======
+      user: {
+>>>>>>> c870c86c (configurer le system des tickets)
         login: string;
         name: string;
         image: string;
         email: string;
+<<<<<<< HEAD
          // Add other custom properties if needed
     };
     accessToken: string; // Add access token property
@@ -16,13 +21,26 @@ declare module 'next-auth' {
 }
 
 declare module 'next-auth' {
+=======
+        // Add other custom properties if needed
+      };
+      accessToken: string; // Add access token property
+    }
+  }
+
+  declare module 'next-auth' {
+>>>>>>> c870c86c (configurer le system des tickets)
     interface User {
         login: string;
         name: string;
         image: string;
         email: string;
     }
+<<<<<<< HEAD
 }
+=======
+  }
+>>>>>>> c870c86c (configurer le system des tickets)
 
 const handler = NextAuth({
     providers: [
@@ -32,6 +50,7 @@ const handler = NextAuth({
         }),
     ],
     callbacks: {
+<<<<<<< HEAD
         async session({ session, token, user }:{session: any, token: any, user: any}) {
             session.accessToken = token.accessToken;
 
@@ -49,6 +68,25 @@ const handler = NextAuth({
         return token;
         },
     },
+=======
+        async session({ session, token, user }) {
+          session.accessToken = token.accessToken;
+        
+        //   console.log(session);
+          return session;
+        },
+        async jwt({ token, user, account, profile, isNewUser }) {
+          if (account) {
+            token.accessToken = account.access_token;
+            // console.log(token)
+          }
+          else{
+            console.log("ERRROR")
+          }
+          return token;
+        },
+      },
+>>>>>>> c870c86c (configurer le system des tickets)
 });
 
 export { handler as GET, handler as POST };
--- src/components/GithubButton.tsx
+++ src/components/GithubButton.tsx
@@ -4,7 +4,11 @@ import { useState } from "react";
 
 export const GithubButton = () => {
     const [loading, setLoading] = useState(false);
-
+    const handleClick = async () => {
+        setLoading(true); // Set loading state to true before sign-in process
+        await signIn("github"); // Perform sign-in process
+        setLoading(false); // Reset loading state after sign-in process completes
+    };
     return (
         <button onClick={() => signIn("github")} 
         className="shrink-0 mt-4 relative bg-transparent text-white rounded-lg ring-2 ring-[#00ff41] px-10 py-4 flex items-center justify-center font-medium  hover:scale-105 transition-all "
