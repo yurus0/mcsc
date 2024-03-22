d270341d7979a7a0b876195b01d1ddefdbad01cf
yurus0 <imanebenrazzouk0302@gmail.com>
2024-03-22 11:45:14 +0000

    feat: fixing fetched data and format;

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,54 +1,57 @@
 import { User } from "next-auth";
+import { User } from "next-auth";
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 
 declare module 'next-auth' {
     interface Session {
-      user: {
+    user: {
         login: string;
         name: string;
         image: string;
         email: string;
-        // Add other custom properties if needed
-      };
-      accessToken: string; // Add access token property
+         // Add other custom properties if needed
+    };
+    accessToken: string; // Add access token property
     }
-  }
+}
 
-  declare module 'next-auth' {
+declare module 'next-auth' {
     interface User {
         login: string;
         name: string;
         image: string;
         email: string;
     }
-  }
+}
 
 const handler = NextAuth({
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID as any,
             clientSecret: process.env.GITHUB_CLIENT_SECRET as any,
+            clientId: process.env.GITHUB_CLIENT_ID as any,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as any,
         }),
     ],
     callbacks: {
-        async session({ session, token, user }: { session: any; token: any; user: User }) {
-          session.accessToken = token.accessToken;
-        
-        //   console.log(session);
-          return session;
+        async session({ session, token, user }:{session: any, token: any, user: any}) {
+            session.accessToken = token.accessToken;
+
+         //   console.log(session);
+            return session;
         },
         async jwt({ token, user, account, profile, isNewUser }) {
-          if (account) {
+        if (account) {
             token.accessToken = account.access_token;
-            // console.log(token)
-          }
-          else{
+             // console.log(token)
+        }
+        else{
             console.log("ERRROR")
-          }
-          return token;
+        }
+        return token;
         },
-      },
+    },
 });
 
 export { handler as GET, handler as POST };
