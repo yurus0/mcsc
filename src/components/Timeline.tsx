a76ae32a26781dd5f7823e6d0d1e408ced0b90e5
yurus0 <imanebenrazzouk0302@gmail.com>
2024-04-02 19:00:34 +0000

    fix: fixing timeline visibility;

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -7,61 +7,115 @@ import 'react-vertical-timeline-component/style.min.css';
 const Timeline = () => {
     const [sessionData, setSessionData] = useState([
         {
-            question: "Opening Ceremony",
-            answer: "Speaker: Speaker Name Description: Session Description",
-            date: "23 April 2024, 4:00 PM",
-            isExpanded: true
+            question: "Keynote Opening",
+            type: "Opening",
+            answer: "",
+            speakers: ["Speaker 1", "Speaker 2", "Speaker 3"],
+            date: "23 April 2024, 3:00 PM",
+            isExpanded: false
         },
         {
             question: "Cloud Compliance: Understanding the Critical role of regulations",
-            answer: "Speaker: Speaker Name Description: Session Description",
-            date: "23 April 2024, 6:00 PM",
-            isExpanded: true
+            type: "Panel Discussion",
+            answer: "Speakers",
+            speakers: ["Speaker 1", "Speaker 2", "Speaker 3"],
+            date: "23 April 2024, 4:00 PM",
+            isExpanded: false
         },
         {
-            question: "Session 3: Date and Time",
-            answer: "Speaker: Speaker Name Description: Session Description",
-            date: "24 April 2024, 4:00 PM",
-            isExpanded: true
+            question: "Cloud-Native Applications: What to Secure?",
+            type: "Conference",
+            answer: "Speakers",
+            speakers: ["Speaker 1", "Speaker 2", "Speaker 3"],
+            date: "24 April 2024, 10:00 AM",
+            isExpanded: false
         },
         {
-            question: "Session 4: Date and Time",
-            answer: "Speaker: Speaker Name Description: Session Description",
-            date: "24 April 2024, 6:00 PM",
-            isExpanded: true
+            question: "Container Security: A Deep Dive",
+            type: "Workshop",
+            answer: "Speakers",
+            speakers: ["Speaker 1", "Speaker 2", "Speaker 3"],
+            date: "24 April 2024, 12:00 PM",
+            isExpanded: false
         },
         {
-            question: "Session 5: Date and Time",
-            answer: "Speaker: Speaker Name Description: Session Description",
+            question: "The convergence of Cloud & Intelligent systems: A new era of security",
+            type: "Conference",
+            answer: "Speakers",
+            speakers: ["Speaker 1", "Speaker 2", "Speaker 3"],
+            date: "24 April 2024, 2:00 PM",
+            isExpanded: false
+        },
+        {
+            question: "AI-Powered Threat Detection for Everyday IoT Devices",
+            type: "Workshop",
+            answer: "Speakers",
+            speakers: ["Speaker 1", "Speaker 2", "Speaker 3"],
             date: "25 April 2024, 4:00 PM",
-            isExpanded: true
+            isExpanded: false
+        },
+        {
+            question: "PenTesting in the Cloud: A Comprehensive Guide",
+            type: "Workshop",
+            answer: "Speakers",
+            speakers: ["Speaker 1", "Speaker 2", "Speaker 3"],
+            date: "25 April 2024, 6:00 PM",
+            isExpanded: false
+        },
+        {
+            question: "The Grand Finale",
+            type: "CTF",
+            answer: "Duration: 12 hours",
+            speakers: ["Speaker 1", "Speaker 2", "Speaker 3"],
+            date: "25 April 2024, 9:00 PM",
+            isExpanded: false
+        },
+        {
+            question: "Awarding Ceremony",
+            type: "Closing",
+            answer: "",
+            speakers: ["Speaker 1", "Speaker 2", "Speaker 3"],
+            date: "25 April 2024, 11:00 AM",
+            isExpanded: false
         },
     ]);
 
+    const toggleSession = (index:any) => {
+        console.log('clicked');
+        setSessionData(sessionData.map((session, i) => {
+            if (i === index) {
+                return { ...session, isExpanded: !session.isExpanded };
+            }
+            return session;
+        }));
+    };
 
     return(
-        <div className="w-full flex flex-col">
-            <div className=" ring-2 ring-[#00ff41] w-full flex p-4 rounded-lg">
+        <div className="w-full h-full flex flex-col">
+            <div className=" ring-4 ring-[#00ff41] w-full flex p-4 rounded-lg py-6">
                 <div className="flex justify-between items-start">
-                    <h1 className="text-white text-4xl pr-20"><code>Timeline</code></h1>
-                    <code>Mark your calendar and make the most out of our event! <br/> See y'all there!</code>
+                    <h1 className="font-bold text-4xl pr-20 pl-6 bg-gradient-to-bl from-white to-[#00ff41] text-transparent inline-box bg-clip-text"><code>Timeline</code></h1>
+                    <code>Mark your calendar and make the most out of our event! <br/>See y'all there!</code>
                 </div>
             </div>
-            <div className='flex flex-row justify-center p-6'>
-                <VerticalTimeline>
+            <div className='flex flex-row justify-center'>
+                <VerticalTimeline animate={true} lineColor={'#00ff41'}>
                     {sessionData.map((session, index) => (
                         <VerticalTimelineElement
                             key={index}
-                            className='hover:cursor-default'
+                            className='visible hover:cursor-default'
                             contentArrowStyle={{ borderRight: '1px solid #000000' }}
-                            contentStyle={{ background: 'transparent', color: '#ffffff', fontWeight: 'lighter'}}
+                            contentStyle={{ background: 'transparent', color: '#ffffff'}}
                             date={session.date}
-                            iconStyle={{ background: '#00ff41', color: '#00ff41', opacity: '0.5'}}
-                            onTimelineElementClick={() => {console.log('clicked'); session.isExpanded = !session.isExpanded; console.log(session.isExpanded); setSessionData([...sessionData])}}
+                            iconStyle={{ background: '#000000', border: '1px solid #00ff41', color: '#00ff41'}}
+                            onTimelineElementClick={() => toggleSession(index)}
+                            visible={true}
                         >
                             <div className='flex flex-col'>
                             <div className='flex flex-row justify-between'>
-                                <h3 className="text-md font-bold vertical-timeline-element-title"><code>{session.question}</code></h3>
+                                <div className='flex flex-col'>
+                                <h3 className="text-lg font-bold vertical-timeline-element-title"><code>{session.question}</code></h3>
+                                <h4 className="text-sm font-light vertical-timeline-element-subtitle pt-2 pb-4"><code>{session.type}</code></h4></div>
                                 {session.isExpanded ? (
                                     <ChevronDownIcon className='w-1 h-1 md:w-6 md:h-6 hover:cursor-pointer' />
                                 ) : (
@@ -69,7 +123,8 @@ const Timeline = () => {
                                 )}
                             
                             </div>
-                            { session.isExpanded && <p className='text-xs font-light vertical-timeline-element-substitle'><code>{session.answer}</code></p>}
+                            { session.isExpanded &&  <h5 className='text-md font-light vertical-timeline-element-substitle'><code>{session.answer}</code></h5>} 
+                            {session.isExpanded && session.speakers.map((speaker, index)=>(<h6 className='text-sm font-light vertical-timeline-element-substitle pt-2'><code>{speaker}</code></h6>))}
                             </div>
                         </VerticalTimelineElement>
                     ))}
